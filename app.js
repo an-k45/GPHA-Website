8fce7e6aa42b5dbb40dde0f90db056e21783aadc
Aniket <39321546+an-k45@users.noreply.github.com>
2018-08-28 11:48:16 -0600

    Routing and page setup

--- app.js
+++ app.js
@@ -8,8 +8,44 @@ app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 
 // ROUTES
-app.get("/", function(req, res) {
+app.get("/", (req, res) => {
   res.render("index");
+})
+
+app.get("/about", (req, res) => {
+  res.render("about");
+})
+
+app.get("/posts", (req, res) => {
+  res.render("posts");
+})
+
+app.get("/membership", (req, res) => {
+  res.render("membership");
+})
+
+app.get("/photos", (req, res) => {
+  res.render("photos");
+})
+
+app.get("/calendar", (req, res) => {
+  res.render("calendar");
+})
+
+app.get("/prayers", (req, res) => {
+  res.render("prayers");
+})
+
+app.get("/community", (req, res) => {
+  res.render("community");
+})
+
+app.get("/donations", (req, res) => {
+  res.render("donations");
+})
+
+app.get("/contact", (req, res) => {
+  res.render("contact");
 })
 
 // Verifies server being online
